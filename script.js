0cd501384a627cf41f1c8aa3f001bca8bb12134a
torolero <42136673+Torolero@users.noreply.github.com>
2026-08-31 17:06:43 -0700

    Add js skeleton
    Added an empty object, constructor, function.

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(name, author) {
+  // the constructor...
+  this.name = name;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
